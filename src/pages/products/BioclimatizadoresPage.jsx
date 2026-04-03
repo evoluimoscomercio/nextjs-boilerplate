8246eb5e8b7297f2b24254db14ca95fd1f99c1e1
Maria <evoluimoscomercio@gmail.com>
2026-04-03 23:14:52 +0100

    g

--- src/pages/products/BioclimatizadoresPage.jsx
+++ src/pages/products/BioclimatizadoresPage.jsx
@@ -23,7 +23,7 @@ export default function BioclimatizadoresPage() {
       />
       <div className="min-h-screen">
         <div className="relative h-80 md:h-[500px] overflow-hidden">
-          <img src="/Bioclimatizadores/bioclimatizadores.png" alt="Bioclimatizadores arrefecimento natural" className="w-full h-full object-cover object-bottom" loading="eager" />
+          <img src="/Bioclimatizadores/bioclimatizadores.png" alt="Bioclimatizadores arrefecimento natural" className="w-full h-full object-cover object-[center_70%]" loading="eager" />
           <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-black/50 to-black/80" />
           <div className="absolute inset-0 flex flex-col">
             <div className="flex justify-center pt-3 px-4">
